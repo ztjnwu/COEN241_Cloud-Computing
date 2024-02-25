2e720f0392fbe98e2306cd591c10d887d53ebe2b
peter zhu <35001024+ztjnwu@users.noreply.github.com>
2024-02-25 05:17:23 +0000

    02-24

--- HW2/chat-bot/handler.py
+++ HW2/chat-bot/handler.py
@@ -2,28 +2,31 @@ import json
 import os
 import requests
 import re
+from time import gmtime, strftime
 
 
 def handle(req):
+    #Get the data from user side
     try:
         data  = json.loads(req)
-        regex = data['question']
-        strings = ['name', 'time', 'figlet']
-        value = ''
-        for string in strings: 
-            if re.match(regex, string):
-                value = string 
-                print(f'Matched: {string}')
-                break
-        
+        query = data['question'].lower()
     except (KeyError, ValueError):
         return json.dumps({"error": "Invalid request payload"})
+    
+    #Build the result for the query from a user
+    result = {}
+    if re.search(r'name', query):
+        result = {"name" : "chatbot"}
+    elif re.search(r'time', query):
+        result = {"time" : strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())}
+    else:
+        url = f"http://127.0.0.1:8080/function/figlet"
+        response = requests.get(url)
+        if response.status_code != 200:
+            return json.dumps({"error": "Error fetching weather data"})
 
-    url = f"http://127.0.0.1:8080/function/figlet"
-    response = requests.get(url)
-    if response.status_code != 200:
-        return json.dumps({"error": "Error fetching weather data"})
-
-    figlet_data = response.json()
-
-    return json.dumps({"figlet": figlet_data})
+        print(json.loads(response.json()))
+        result = {"figlet" : json.loads(response.json())}
+        
+    #Return
+    return json.dumps(result)
